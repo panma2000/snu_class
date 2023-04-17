a72eb08a4c7834cb72da2b7a96dc2cc2c656c67c
panma2000 <127073813+panma2000@users.noreply.github.com>
2023-04-17 14:20:19 +0900

    .
    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-// vite.config.js
+import { defineConfig } from "vite";
 
-export default {
-    build: {
-      outDir: 'docs'
+export default defineConfig({
+  // 프로젝트가 호스팅되는 GitHub Pages의 저장소 이름으로 경로를 설정합니다.
+  // 예를 들어, 저장소 이름이 'my-project'라면 다음과 같이 설정합니다.
+  base: '/snu_class/',
+  build: {
+  outDir: 'docs',
+  manifest: true, // Enable the generation of the manifest file
+  rollupOptions: {
+    output: {
+      // Use `./` as the asset file prefix
+      assetFileNames: `[name].[hash].[ext]`,
+      chunkFileNames: `[name].[hash].js`,
+      entryFileNames: `[name].[hash].js`,
     }
-  }
+  },
+    publicPath: '/snu_class/'
+  },
+});
